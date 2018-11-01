07934fdcb81bd1368ccbb1667066aa40f5d9ffb4
Kevin Lane <lane.kevin.a@gmail.com>
2018-10-31 22:08:19 -0700

    Add notebook conversion test

--- tests/test_jekyllnb.py
+++ tests/test_jekyllnb.py
@@ -1,5 +1,33 @@
+import filecmp
+import os
+import pytest
 from jekyllnb.jekyllnb import jekyllpath
+from nbconvert.nbconvertapp import NbConvertApp
 
+
+FILE_NAME = 'hello-world'
+
+@pytest.fixture
+def test_file(tmpdir):
+    test_file_name = tmpdir.join(FILE_NAME + '.md')
+
+    NbConvertApp.launch_instance(['--to', 'jekyll', '--output-dir', test_file_name.dirname,
+        os.path.join(os.path.dirname(__file__), 'resources', FILE_NAME + '.ipynb')])
+
+    contents = test_file_name.read()
+
+    with open(test_file_name, 'w', newline='\n') as f:
+        f.write(contents)
+
+    return test_file_name
+
+def test_file_exists(test_file):
+    assert test_file.check()
+
+#TODO ignore whitespace in comparison or remove it after print statement in conversion
+def test_jekyllnb(test_file):
+    target_file = os.path.join(os.path.dirname(__file__), 'resources', FILE_NAME + '.md')
+    assert filecmp.cmp(test_file, target_file, shallow=False)
 
 def test_jekyllpath():
     assert jekyllpath('assets\\images') == '{{ site.baseurl }}/assets/images'
