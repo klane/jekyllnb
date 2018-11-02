cdcb1118cf0566d4ce29deb642ce5d936ce93358
Kevin Lane <lane.kevin.a@gmail.com>
2018-11-01 19:02:00 -0700

    Add pretty print of test file diff when file comparison fails

--- tests/test_jekyllnb.py
+++ tests/test_jekyllnb.py
@@ -1,8 +1,10 @@
 import filecmp
 import os
 import pytest
+from difflib import Differ
 from jekyllnb.jekyllnb import jekyllpath
 from nbconvert.nbconvertapp import NbConvertApp
+from pprint import pprint
 
 
 FILE_NAME = 'hello-world'
@@ -27,7 +29,15 @@ def test_file_exists(test_file):
 #TODO ignore whitespace in comparison or remove it after print statement in conversion
 def test_jekyllnb(test_file):
     target_file = os.path.join(os.path.dirname(__file__), 'resources', FILE_NAME + '.md')
-    assert filecmp.cmp(test_file, target_file, shallow=False)
+
+    try:
+        assert filecmp.cmp(test_file, target_file, shallow=False)
+    except AssertionError:
+        with open(target_file) as target:
+            differ = Differ()
+            diff = differ.compare(test_file.readlines(), target.readlines())
+            pprint(list(diff))
+            raise
 
 def test_jekyllpath():
     assert jekyllpath('assets\\images') == '{{ site.baseurl }}/assets/images'
