ed73e20f90fa934581e5c981598aadc9e3abfec3
Kevin Lane <lane.kevin.a@gmail.com>
2018-11-11 10:13:16 -0800

    Add tests for calling jekyllnb through nbconvert command line

--- tests/test_nbconvert.py
+++ tests/test_nbconvert.py
@@ -1,3 +1,5 @@
+from nbconvert.nbconvertapp import NbConvertApp
+from subprocess import call
 from tests import *
 
 
@@ -11,35 +13,43 @@ def image_dir(site_dir):
 
 @pytest.fixture
 def nbconvert_file(site_dir):
-    from nbconvert.nbconvertapp import NbConvertApp
-    NbConvertApp.launch_instance([
+    return site_dir.join(FILE_NAME + '.md')
+
+@pytest.fixture
+def nbconvert_execute(site_dir, request):
+    params = [
         '--to', 'jekyll',
         '--output-dir', site_dir.strpath,
         '--NbConvertApp.output_files_dir=' + IMAGE_DIR,
         os.path.join(os.path.dirname(__file__), 'resources', FILE_NAME + '.ipynb')
-    ])
+    ]
 
-    yield site_dir.join(FILE_NAME + '.md')
+    request.param(params)
     NbConvertApp.clear_instance()
 
-def test_nbconvert_file_exists(nbconvert_file):
-    assert nbconvert_file.check()
+@pytest.mark.parametrize('nbconvert_execute', [
+    lambda params: NbConvertApp.launch_instance(params),
+    lambda params: call(['jupyter', 'nbconvert'] + params)
+], indirect=True)
+class TestNbConvert(object):
+    def test_nbconvert_file_exists(self, nbconvert_execute, nbconvert_file):
+        assert nbconvert_file.check()
 
-def test_nbconvert_image_exists(nbconvert_file, image_dir):
-    assert os.path.isdir(image_dir.strpath)
-    assert os.path.isfile(image_dir.join(FILE_NAME + '_4_0.png').strpath)
+    def test_nbconvert_image_exists(self, nbconvert_execute, image_dir):
+        assert os.path.isdir(image_dir.strpath)
+        assert os.path.isfile(image_dir.join(FILE_NAME + '_4_0.png').strpath)
 
-def test_nbconvert_file_contents(nbconvert_file):
-    test_lines = nbconvert_file.readlines()
-    target_file = os.path.join(os.path.dirname(__file__), 'resources', FILE_NAME + '.md')
+    def test_nbconvert_file_contents(self, nbconvert_execute, nbconvert_file):
+        test_lines = nbconvert_file.readlines()
+        target_file = os.path.join(os.path.dirname(__file__), 'resources', FILE_NAME + '.md')
 
-    with open(target_file) as target:
-        target_lines = target.readlines()
+        with open(target_file) as target:
+            target_lines = target.readlines()
 
-    try:
-        assert all(a == b for a, b in zip(test_lines, target_lines))
-    except AssertionError:
-        differ = Differ()
-        diff = differ.compare(test_lines, target_lines)
-        pprint(list(diff))
-        raise
+        try:
+            assert all(a == b for a, b in zip(test_lines, target_lines))
+        except AssertionError:
+            differ = Differ()
+            diff = differ.compare(test_lines, target_lines)
+            pprint(list(diff))
+            raise
