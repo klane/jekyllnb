41623459d736898ae3fa2d12efa7bd30bfc86117
Kevin Lane <lane.kevin.a@gmail.com>
2019-05-22 22:12:19 -0700

    Simplify preprocessor

--- jekyllnb/preprocessor.py
+++ jekyllnb/preprocessor.py
@@ -4,13 +4,12 @@ from nbconvert.preprocessors import Preprocessor
 class JekyllPreprocessor(Preprocessor):
     def preprocess(self, nb, resources):
         name = resources['metadata']['name']
-        resources['metadata']['jekyll'] = {'layout': 'page',
-                                           'title': name,
-                                           'permalink': '/' + name
-                                           }
-        metadata = nb.metadata.get('jekyll', {})
-
-        for key in metadata:
-            resources['metadata']['jekyll'][key] = metadata[key]
+        metadata = {
+            'layout': 'page',
+            'title': name,
+            'permalink': '/' + name
+        }
+        metadata.update(nb.metadata.get('jekyll', {}))
+        resources['metadata']['jekyll'] = metadata
 
         return nb, resources
