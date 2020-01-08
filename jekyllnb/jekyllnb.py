de2d398afa3bc5ccd4f31de90f971b8a219a9d82
Kevin Lane <lane.kevin.a@gmail.com>
2020-01-07 19:28:39 -0800

    Revert "Move variable static types to comments"
    This reverts commit 9d708aa4291aacacfd09cd83873caa9a715f175a.

--- jekyllnb/jekyllnb.py
+++ jekyllnb/jekyllnb.py
@@ -7,7 +7,7 @@ from traitlets.config import catch_config_error
 
 from .__version__ import __version__
 
-JEKYLLNB_ALIASES = {}  # type: Dict[str, str]
+JEKYLLNB_ALIASES: Dict[str, str] = {}
 JEKYLLNB_ALIASES.update(nbconvert_aliases)
 JEKYLLNB_ALIASES.update(
     {
@@ -17,7 +17,7 @@ JEKYLLNB_ALIASES.update(
     }
 )
 
-JEKYLLNB_FLAGS = {}  # type: Dict[str, Sequence[Any]]
+JEKYLLNB_FLAGS: Dict[str, Sequence[Any]] = {}
 JEKYLLNB_FLAGS.update(nbconvert_flags)
 JEKYLLNB_FLAGS.update(
     {
