afc7ac6cff3ac44d7421ab288262b267a3997fe6
Kevin Lane <lane.kevin.a@gmail.com>
2020-10-24 10:18:49 -0700

    Reorder imports to resolve circular import

--- jekyllnb/__init__.py
+++ jekyllnb/__init__.py
@@ -1,6 +1,6 @@
 from .__version__ import __version__
 from .exporter import JekyllExporter
-from .jekyllnb import JekyllNB
 from .preprocessor import JekyllPreprocessor
+from .jekyllnb import JekyllNB
 
 __all__ = ["__version__", "JekyllExporter", "JekyllNB", "JekyllPreprocessor"]
