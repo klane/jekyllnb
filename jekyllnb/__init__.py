df7876a4d4cfcb8edc66610834ebdc100202c05d
Kevin Lane <lane.kevin.a@gmail.com>
2020-10-24 11:11:31 -0700

    Skip isort check on reordered import

--- jekyllnb/__init__.py
+++ jekyllnb/__init__.py
@@ -1,6 +1,7 @@
 from .__version__ import __version__
 from .exporter import JekyllExporter
 from .preprocessor import JekyllPreprocessor
-from .jekyllnb import JekyllNB
+
+from .jekyllnb import JekyllNB  # isort: skip
 
 __all__ = ["__version__", "JekyllExporter", "JekyllNB", "JekyllPreprocessor"]
