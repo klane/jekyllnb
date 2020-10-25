3bdbd1f1ce468e5dfcbb5506d5f595ed5bbfce23
Kevin Lane <lane.kevin.a@gmail.com>
2020-10-24 17:07:02 -0700

    Remove JekyllNB as a top level import
    Resolves import issue when running with nbconvert version 6

--- jekyllnb/__init__.py
+++ jekyllnb/__init__.py
@@ -2,6 +2,4 @@ from .__version__ import __version__
 from .exporter import JekyllExporter
 from .preprocessor import JekyllPreprocessor
 
-from .jekyllnb import JekyllNB  # isort: skip
-
-__all__ = ["__version__", "JekyllExporter", "JekyllNB", "JekyllPreprocessor"]
+__all__ = ["__version__", "JekyllExporter", "JekyllPreprocessor"]
