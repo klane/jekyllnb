f72a3ee5cd29ccab89ddc5ea6b7101e32b84b600
Kevin Lane <lane.kevin.a@gmail.com>
2024-10-12 12:56:35 -0700

    Update source

--- jekyllnb/jekyllnb.py
+++ jekyllnb/jekyllnb.py
@@ -1,5 +1,6 @@
 import os
-from typing import Any, Dict, Sequence
+from collections.abc import Sequence
+from typing import Any, Literal, Optional
 
 from nbconvert.nbconvertapp import NbConvertApp, nbconvert_aliases, nbconvert_flags
 from traitlets import Bool, Unicode, default, observe
@@ -7,7 +8,7 @@ from traitlets.config import catch_config_error
 
 from .__version__ import __version__
 
-JEKYLLNB_ALIASES: Dict[str, str] = {}
+JEKYLLNB_ALIASES: dict[str, str] = {}
 JEKYLLNB_ALIASES.update(nbconvert_aliases)
 JEKYLLNB_ALIASES.update(
     {
@@ -17,7 +18,7 @@ JEKYLLNB_ALIASES.update(
     }
 )
 
-JEKYLLNB_FLAGS: Dict[str, Sequence[Any]] = {}
+JEKYLLNB_FLAGS: dict[str, Sequence[Any]] = {}
 JEKYLLNB_FLAGS.update(nbconvert_flags)
 JEKYLLNB_FLAGS.update(
     {
@@ -30,7 +31,7 @@ JEKYLLNB_FLAGS.update(
 
 
 class JekyllNB(NbConvertApp):
-    """Application to convert notebooks (``*.ipynb``) to Jekyll Markdown (``.md``)"""
+    """Application to convert notebooks (``*.ipynb``) to Jekyll Markdown (``.md``)."""
 
     name = "jupyter-jekyllnb"
     description = "Convert Jupyter notebooks to Jekyll-ready Markdown"
@@ -46,13 +47,13 @@ class JekyllNB(NbConvertApp):
     ).tag(config=True)
 
     @default("export_format")
-    def _export_format_default(self):  # skipcq: PYL-R0201
-        """Default export format"""
+    def _export_format_default(self) -> Literal["jekyll"]:
+        """Specify default export format."""
         return "jekyll"
 
     @observe("export_format")
-    def _export_format_changed(self, change):
-        """Ensure export format is jekyll"""
+    def _export_format_changed(self, change: dict[str, Any]) -> None:
+        """Ensure export format is jekyll."""
         default_format = self._export_format_default()
 
         if change["new"].lower() != default_format:
@@ -61,18 +62,18 @@ class JekyllNB(NbConvertApp):
             )
 
     @catch_config_error
-    def initialize(self, argv=None):
-        """Initialize application, notebooks, writer, and postprocessor"""
+    def initialize(self, argv: Optional[Any] = None) -> None:
+        """Initialize application, notebooks, writer, and postprocessor."""
         super().initialize(argv)
         self.writer.build_directory = os.path.join(self.site_dir, self.page_dir)
 
         if self.auto_folder:
             self.output_files_dir = os.path.join(
-                self.output_files_dir, "{notebook_name}"
+                super().output_files_dir, "{notebook_name}"
             )
 
-    def init_single_notebook_resources(self, notebook_filename):
-        """Initialize resources
+    def init_single_notebook_resources(self, notebook_filename: str) -> dict[str, Any]:
+        """Initialize resources.
 
         Initializes the resources dictionary for a single notebook.
 
@@ -86,6 +87,7 @@ class JekyllNB(NbConvertApp):
                 - config_dir: location of the Jupyter config directory
                 - unique_key: notebook name
                 - output_files_dir: directory where output files should be saved
+
         """
         resources = super().init_single_notebook_resources(notebook_filename)
         resources["site_dir"] = os.path.join(os.getcwd(), self.site_dir)
